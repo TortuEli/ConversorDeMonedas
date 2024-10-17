c5539d4e3ab5e8e7d3c4ff76aa02b532747938af
elizabeth.lavenant <elizabeth.lavenant@semarnat.gob.mx>
2024-10-17 13:32:29 -0600

    inauguramos repositorio del challenge

---
+++ src/main/java/com/challenge/conversordemonedas/ConversorDeMonedas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.challenge.conversordemonedas;
+
+/**
+ *
+ * @author elizabeth.lavenant@dominio.semarnat.gob.mx
+ */
+public class ConversorDeMonedas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
